0176adc08713a31af353ea816be8be91a722fe90
motaz ahmed Rawi <motazalrawi2002@gmail.com>
2023-12-24 21:00:42 +0300

    cmt

--- servisleme/servisleme.py
+++ servisleme/servisleme.py
@@ -7,30 +7,30 @@ import joblib
 import streamlit as st
 import matplotlib.pyplot as plt
 
-# Veri setini oku
+# Veri setini okuma
 file_path = "../veritoplama/processed_matches.csv"
 df = pd.read_csv(file_path)
 
-# "team_B" sütununu Label Encoding yap
+# "team_B" sütununu Label Encoding yapma
 label_encoder = LabelEncoder()
 df['team_B_encoded'] = label_encoder.fit_transform(df['team_B'])
 
-# Load the trained model
+# eğitilmiş modeli yükleme(okuma)
 best_model = joblib.load('../algoritmalar/model.pkl')
 
-# Streamlit arayüzü
+
 st.title("Real Madrid Win Prediction Model")
 
-# Kullanıcıdan takım seçimini al
+# Kullanıcıdan takım alma
 selected_team = st.selectbox("Select a team:", df['team_B'].unique())
 
-# Label Encoding uygula
+# seçilen takımı sayısallaştırma
 selected_team_encoded = label_encoder.transform([selected_team])
 
-# Tahmin olasılıklarını al
+# Tahmin olasılıklarını alma
 probabilities = best_model.predict_proba([[selected_team_encoded[0]]])[0]
 
-# Olasılıkları göster
+# Olasılıkları gösterme
 st.write(f"{selected_team} Team Next Match Probabiliteis:")
 st.write(f"Probability of Real Madrid winning: {probabilities[1]:.2%}")
 st.write(f"Draw Probability: {probabilities[2]:.2%}")
@@ -39,9 +39,9 @@ st.write(f"Probability of {selected_team} winning: {probabilities[0]:.2%}")
 # Maç istatistikleri
 team_A_wins = len(df[(df['team_B'] == selected_team) & (df['score_A'] > df['score_B'])])
 team_B_wins = len(df[(df['team_B'] == selected_team) & (df['score_A'] < df['score_B'])])
-draws = len(df[(df['team_B'] == selected_team) & (df['score_A'] == df['score_B'])])
+draws       = len(df[(df['team_B'] == selected_team) & (df['score_A'] == df['score_B'])])
 
-# Maç istatistikleri
+# Maç istatistikleri yazdırmak
 st.subheader("Match Statistics")
 st.write(f"Real Madrid Wins: {team_A_wins} times")
 st.write(f"{selected_team} Wins: {team_B_wins} times")
@@ -51,10 +51,11 @@ st.write(f"Draws: {draws} times")
 st.sidebar.header('Graph Menu')
 st.sidebar.title('Statistics Chart')
 
-# Calculate the maximum probability for the selected team's future match outcome
+# seçilen takımın gelecek maç sonucu için maksimum olasılığı hesaplamak
 prediction_probability = max(probabilities)
 
-# Sidebar'da çubuk grafik gösterimi
+# Sidebar'da çubuk grafik gösterimi 
+
 with st.sidebar.expander('Match Statistics'):
     fig, ax = plt.subplots()
     labels = ['Real Madrid Wins', f'{selected_team} Wins', 'Draws']
@@ -66,10 +67,11 @@ with st.sidebar.expander('Match Statistics'):
 
     st.pyplot(fig)
 
-# Sidebar menü
+
 st.sidebar.title('Prediction Chart')
 
 # Sidebar'da pasta grafiği gösterimi
+
 with st.sidebar.expander('Match Prediction'):
     labels = ['Real Madrid Wins', f'{selected_team} Wins', 'Draws']
     sizes = [probabilities[1], probabilities[0], probabilities[2]]
